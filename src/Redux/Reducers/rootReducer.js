cca4ad8a7d12a055c270589a60813dfd2d58e2a2
Kaustubh-Scube <dev16@scube.net.in>
2025-03-18 18:03:39 +0530

    Update: Remove old config and package

--- src/Redux/Reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import InstituteReducer from './InstituteReducer'
-import VerifierReducer from './VerifierReducer'
-
-const reducer = combineReducers({
-    InstituteReducer: InstituteReducer,
-    VerifierReducer: VerifierReducer
-})
-export default reducer;
